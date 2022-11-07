87a75ebe85fca350a56f30392703606f7035b855
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 13:57:07 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.scss';
+import translations from './data/translations.json';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Excel Read/Write Example</h1>
 			<p>Welcome to this site.</p>
+
+			<h2>There are {translations.length} translations:</h2>
+
+			<table className="blueTable">
+				<tbody>
+				{translations.map((translation, i) => {
+					return (
+						<tr className="translation" key={i}>
+							<td className="fromLanguage">
+								{translation.fromLanguage}
+							</td>
+							<td className="toLanguage">
+								{translation.toLanguage}
+							</td>
+							<td className="toPhrase">
+								{translation.toPhrase}
+							</td>
+							<td className="fromPhrase">
+								{translation.fromPhrase}
+							</td>
+						</tr>
+					);
+				})}
+				</tbody>
+			</table>
 		</div>
 	);
 }
