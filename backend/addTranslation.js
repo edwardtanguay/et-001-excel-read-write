2dc59ff560d42746131edd750ec1706464a4e321
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 14:19:15 +0100

    finished 2

--- backend/addTranslation.js
+++ backend/addTranslation.js
@@ -1,18 +1,20 @@
 import Excel from 'exceljs';
 
+const fileName = './src/data/translations.xlsx';
+
 const wb = new Excel.Workbook();
-await wb.xlsx.readFile('./src/data/translations.xlsx');
+await wb.xlsx.readFile(fileName);
 const ws = wb.getWorksheet(1);
 
-ws.getCell('A1').value = 'test';
+// ws.getCell('A1').value = 'test';
 
-const r3 = ws.getRow(3);
-r3.values = [1, 2, 3, 4, 5, 6];
+const r3 = ws.getRow(3119);
+r3.values = ['English', 'German', 'house', 'das House'];
 
 wb.xlsx
 	.writeFile(fileName)
 	.then(() => {
-		console.log('file created');
+		console.log('file changed');
 	})
 	.catch((err) => {
 		console.log(err.message);
